1c12a707fb8195d5bd53862409b72d4a7de8c624
Jozsef Dojcsak <jozsef.dojcsak@gmail.com>
2024-05-24 09:49:01 +0200

    Fix VDI card caching problem

--- src/minidriver/minidriver.c
+++ src/minidriver/minidriver.c
@@ -449,19 +449,7 @@ check_card_reader_status(PCARD_DATA pCardData, const char *name)
 		logprintf(pCardData, 1, "HANDLES CHANGED from  vs->hSCardCtx:0x%08"SC_FORMAT_LEN_SIZE_T"X vs->hScard:0x%08"SC_FORMAT_LEN_SIZE_T"X\n",
 			(size_t)vs->hSCardCtx,
 			(size_t)vs->hScard);
-		if (vs->ctx) {
-			if (1 == pcsc_check_reader_handles(vs->ctx, vs->reader, &pCardData->hSCardCtx, &pCardData->hScard)) {
-				_sc_delete_reader(vs->ctx, vs->reader);
-				MD_FUNC_RETURN(pCardData, 1, reinit_card_for(pCardData, name));
-			} else {
-				vs->hScard = pCardData->hScard;
-				vs->hSCardCtx = pCardData->hSCardCtx;
-				r = sc_ctx_use_reader(vs->ctx, &vs->hSCardCtx, &vs->hScard);
-				logprintf(pCardData, 1, "sc_ctx_use_reader returned %d\n", r);
-				if (r)
-					MD_FUNC_RETURN(pCardData, 1, SCARD_F_INTERNAL_ERROR);
-			}
-		}
+		MD_FUNC_RETURN(pCardData, 1, reinit_card_for(pCardData, name));
 	}
 
 	/* This should always work, as BaseCSP should be checking for removal too */
