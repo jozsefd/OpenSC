52d0a8c7802fe694ea16678a9933dc7d3914c930
Jozsef Dojcsak <jozsef.dojcsak@gmail.com>
2024-08-23 18:02:36 +0200

    Fix reader name handling on macOS VDI

--- src/libopensc/reader-pcsc.c
+++ src/libopensc/reader-pcsc.c
@@ -2575,9 +2575,16 @@ int pcsc_use_reader(sc_context_t *ctx, void * pcsc_context_handle, void * pcsc_c
 	gpriv->pcsc_ctx = *(SCARDCONTEXT *)pcsc_context_handle;
 	card_handle = *(SCARDHANDLE *)pcsc_card_handle;
 
-	if(SCARD_S_SUCCESS == gpriv->SCardGetAttrib(card_handle,
+	rc = gpriv->SCardGetAttrib(card_handle,
 				SCARD_ATTR_DEVICE_SYSTEM_NAME_A, (LPBYTE)
-				reader_name, &reader_name_size)) {
+				reader_name, &reader_name_size);
+	if(SCARD_S_SUCCESS != rc) {
+		// falback: if SCARD_ATTR_DEVICE_SYSTEM_NAME_A not supported, trying SCARD_ATTR_DEVICE_FRIENDLY_NAME_A
+		rc = gpriv->SCardGetAttrib(card_handle,
+				SCARD_ATTR_DEVICE_FRIENDLY_NAME_A, (LPBYTE)
+				reader_name, &reader_name_size);
+	}				
+	if(SCARD_S_SUCCESS == rc) {
 		sc_reader_t *reader = NULL;
 
 		ret = pcsc_add_reader(ctx, reader_name, reader_name_size, &reader);
